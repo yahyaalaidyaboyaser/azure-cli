2df779e6cf065c54ef80887fa4beef4e771ec36f
Hang Lei <hanglei@microsoft.com>
2022-08-11 10:42:57 +0800

    Disable binary_cache

--- src/azure-cli-core/azure/cli/core/auth/binary_cache.py
+++ src/azure-cli-core/azure/cli/core/auth/binary_cache.py
@@ -32,7 +32,8 @@ class BinaryCache(collections.MutableMapping):
         """Load cache with retry. If it still fails at last, raise the original exception as-is."""
         try:
             with open(self.filename, 'rb') as f:
-                return pickle.load(f)
+                # windows-http's response is not picklable, so we also don't load it.
+                return {}
         except FileNotFoundError:
             # The cache file has not been created. This is expected. No need to retry.
             logger.debug("%s not found. Using a fresh one.", self.filename)
@@ -55,7 +56,8 @@ class BinaryCache(collections.MutableMapping):
             # At this point, an empty cache file will be created. Loading this cache file will
             # raise EOFError. This can be simulated by adding time.sleep(30) here.
             # So during loading, EOFError is ignored.
-            pickle.dump(self.data, f)
+            # windows-http's response is not picklable, so we don't save it.
+            pass
 
     def save(self):
         logger.debug("save: %s", self.filename)
