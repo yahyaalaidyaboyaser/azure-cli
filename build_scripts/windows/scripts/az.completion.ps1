769be40c54a18f9fdf48ef5b66bec134be2048c1
Hang Lei <hanglei@microsoft.com>
2023-05-17 15:23:02 +0800

    Minor fix

--- build_scripts/windows/scripts/az.completion.ps1
+++ build_scripts/windows/scripts/az.completion.ps1
@@ -10,9 +10,9 @@ Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
     $env:_ARGCOMPLETE_IFS = "`n"
     az 2>&1 | Out-Null
     
-    Get-Content $completion_file | Sort-Object |ForEach-Object {
+    Get-Content $completion_file | Sort-Object | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
     }
-    rm $completion_file
+    Remove-Item $completion_file
     Remove-Item Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS
 }
