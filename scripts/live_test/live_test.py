dd3fba9bb72d58b2d2d6380a124603286d353627
ZelinWang <zelinwang@microsoft.com>
2023-06-08 18:54:39 +0800

    update

--- scripts/live_test/live_test.py
+++ scripts/live_test/live_test.py
@@ -6,16 +6,16 @@
 # --------------------------------------------------------------------------------------------
 
 from azdev.utilities import get_path_table
-from azure.kusto.data import KustoConnectionStringBuilder
-from azure.kusto.data.data_format import DataFormat
-from azure.kusto.ingest import (
-    IngestionProperties,
-    QueuedIngestClient,
-    ReportLevel,
-)
-from bs4 import BeautifulSoup
-import csv
-import datetime
+# from azure.kusto.data import KustoConnectionStringBuilder
+# from azure.kusto.data.data_format import DataFormat
+# from azure.kusto.ingest import (
+#     IngestionProperties,
+#     QueuedIngestClient,
+#     ReportLevel,
+# )
+# from bs4 import BeautifulSoup
+# import csv
+# import datetime
 import logging
 import os
 import subprocess
@@ -414,80 +414,79 @@ class AutomaticScheduling(object):
                 logger.info(parallel)
                 error_flag = run_command(parallel, check_return_code=True)
                 global_error_flag = global_error_flag or error_flag
-            self.generate_csv_file(module)
-            # TODO: add filter for main repo and ext repo
-            if USER_TARGET.lower() in ['all', '']:
-                # self.generate_csv_file(module)
-                self.send_to_kusto(module)
+            # # TODO: add filter for main repo and ext repo
+            # if USER_TARGET.lower() in ['all', '']:
+            #     self.generate_csv_file(module)
+            #     self.send_to_kusto(module)
         return global_error_flag or error_flag
 
-    def generate_csv_file(self, module):
-        logger.info('Start generate csv file for {TARGET}.'.format(TARGET=module))
-        data = []
-        parallel_file = f'{OUTPUT_DIR}/{module}.{OS_VERSION}.report.parallel.html'
-        sequential_file = f'{OUTPUT_DIR}/{module}.{OS_VERSION}.report.sequential.html'
-
-        def _get_data(html_file):
-            data = []
-            if os.path.exists(html_file):
-                with open(html_file) as file:
-                    bs = BeautifulSoup(file, "html.parser")
-                    results = bs.find(id="results-table")
-                    Source = 'LiveTest'
-                    BuildId = BUILD_ID
-                    Module = module
-                    Description = ''
-                    ExtendedProperties = ''
-                    for result in results.find_all('tbody'):
-                        Name = result.find('td', {'class': 'col-name'}).text.split('::')[-1]
-                        Duration = result.find('td', {'class': 'col-duration'}).text
-                        Status = result.find('td', {'class': 'col-result'}).text
-                        if Status == 'Failed':
-                            contents = result.find('td', {'class': 'extra'}).find('div', {'class': 'log'}).contents
-                            Details = ''
-                            for content in contents:
-                                if content.name == 'br':
-                                    Details += '\n'
-                                elif not content.name:
-                                    Details += content
-                                else:
-                                    logger.warning(content.name)
-                        else:
-                            Details = ''
-                        EndDateTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        StartDateTime = (datetime.datetime.now() - datetime.timedelta(seconds=int(float(Duration)))).strftime("%Y-%m-%d %H:%M:%S")
-                        data.append([Source, BuildId, OS_VERSION, PYTHON_VERSION, Module, Name, Description, StartDateTime, EndDateTime, Duration, Status, Details, ExtendedProperties])
-            return data
-
-        data.extend(_get_data(parallel_file))
-        data.extend(_get_data(sequential_file))
-        logger.info('CSV data: {data}'.format(data=data))
-
-        with open(f'{OUTPUT_DIR}/{module}.csv', mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(data)
-        logger.info('Finish generate csv file for {TARGET}.'.format(TARGET=module))
-
-
-    def send_to_kusto(self, module):
-        logger.info('Start send csv data to kusto db for {TARGET}'.format(TARGET=module))
-        kcsb = KustoConnectionStringBuilder.with_aad_application_key_authentication(KUSTO_CLUSTER, KUSTO_CLIENT_ID, KUSTO_CLIENT_SECRET, KUSTO_TENANT_ID)
-        # The authentication method will be taken from the chosen KustoConnectionStringBuilder.
-        client = QueuedIngestClient(kcsb)
-
-        # there are a lot of useful properties, make sure to go over docs and check them out
-        ingestion_props = IngestionProperties(
-            database=KUSTO_DATABASE,
-            table=KUSTO_TABLE,
-            data_format=DataFormat.CSV,
-            report_level=ReportLevel.FailuresAndSuccesses
-        )
-
-        # ingest from file
-        result = client.ingest_from_file(f"{OUTPUT_DIR}/{module}.csv", ingestion_properties=ingestion_props)
-        # Inspect the result for useful information, such as source_id and blob_url
-        print(repr(result))
-        logger.info('Finsh send csv data to kusto db for {}.'.format(module))
+    # def generate_csv_file(self, module):
+    #     logger.info('Start generate csv file for {TARGET}.'.format(TARGET=module))
+    #     data = []
+    #     parallel_file = f'{OUTPUT_DIR}/{module}.{OS_VERSION}.report.parallel.html'
+    #     sequential_file = f'{OUTPUT_DIR}/{module}.{OS_VERSION}.report.sequential.html'
+    #
+    #     def _get_data(html_file):
+    #         data = []
+    #         if os.path.exists(html_file):
+    #             with open(html_file) as file:
+    #                 bs = BeautifulSoup(file, "html.parser")
+    #                 results = bs.find(id="results-table")
+    #                 Source = 'LiveTest'
+    #                 BuildId = BUILD_ID
+    #                 Module = module
+    #                 Description = ''
+    #                 ExtendedProperties = ''
+    #                 for result in results.find_all('tbody'):
+    #                     Name = result.find('td', {'class': 'col-name'}).text.split('::')[-1]
+    #                     Duration = result.find('td', {'class': 'col-duration'}).text
+    #                     Status = result.find('td', {'class': 'col-result'}).text
+    #                     if Status == 'Failed':
+    #                         contents = result.find('td', {'class': 'extra'}).find('div', {'class': 'log'}).contents
+    #                         Details = ''
+    #                         for content in contents:
+    #                             if content.name == 'br':
+    #                                 Details += '\n'
+    #                             elif not content.name:
+    #                                 Details += content
+    #                             else:
+    #                                 logger.warning(content.name)
+    #                     else:
+    #                         Details = ''
+    #                     EndDateTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    #                     StartDateTime = (datetime.datetime.now() - datetime.timedelta(seconds=int(float(Duration)))).strftime("%Y-%m-%d %H:%M:%S")
+    #                     data.append([Source, BuildId, OS_VERSION, PYTHON_VERSION, Module, Name, Description, StartDateTime, EndDateTime, Duration, Status, Details, ExtendedProperties])
+    #         return data
+    #
+    #     data.extend(_get_data(parallel_file))
+    #     data.extend(_get_data(sequential_file))
+    #     logger.info('CSV data: {data}'.format(data=data))
+    #
+    #     with open(f'{OUTPUT_DIR}/{module}.csv', mode='w', newline='') as file:
+    #         writer = csv.writer(file)
+    #         writer.writerows(data)
+    #     logger.info('Finish generate csv file for {TARGET}.'.format(TARGET=module))
+    #
+    #
+    # def send_to_kusto(self, module):
+    #     logger.info('Start send csv data to kusto db for {TARGET}'.format(TARGET=module))
+    #     kcsb = KustoConnectionStringBuilder.with_aad_application_key_authentication(KUSTO_CLUSTER, KUSTO_CLIENT_ID, KUSTO_CLIENT_SECRET, KUSTO_TENANT_ID)
+    #     # The authentication method will be taken from the chosen KustoConnectionStringBuilder.
+    #     client = QueuedIngestClient(kcsb)
+    #
+    #     # there are a lot of useful properties, make sure to go over docs and check them out
+    #     ingestion_props = IngestionProperties(
+    #         database=KUSTO_DATABASE,
+    #         table=KUSTO_TABLE,
+    #         data_format=DataFormat.CSV,
+    #         report_level=ReportLevel.FailuresAndSuccesses
+    #     )
+    #
+    #     # ingest from file
+    #     result = client.ingest_from_file(f"{OUTPUT_DIR}/{module}.csv", ingestion_properties=ingestion_props)
+    #     # Inspect the result for useful information, such as source_id and blob_url
+    #     print(repr(result))
+    #     logger.info('Finsh send csv data to kusto db for {}.'.format(module))
 
 def main():
     logger.info("Start automation full test ...\n")
